d86edab9b1819cab765546292c24ce026945f4a5
KomarRIP <komarik555sasha@gmail.com>
2017-07-17 12:44:34 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ProjectOne
         public static void Main (string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.ReadKey();    
         }
     }
 }
