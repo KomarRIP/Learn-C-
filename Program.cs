9727d8c003e54c4ca5fd37b365797214575f1b9f
KomarRIP <komarik555sasha@gmail.com>
2017-07-17 12:32:58 +0300

    Release 0.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectOne
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
